cf87e893d4e421fae6f981fc028ec8ad43260445
Kunal Jaiswal <kunaljaiswal1nov@gmail.com>
2026-01-27 18:35:49 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,4 +2,11 @@
 // nature of the scaffolded files. This is a representative example.
 //
 // These details will be automatically populated with a call to `RequestFirebaseBackendTool`.
-export const firebaseConfig = {"apiKey":"API_KEY","authDomain":"PROJECT_ID.firebaseapp.com","projectId":"PROJECT_ID","storageBucket":"PROJECT_ID.appspot.com","messagingSenderId":"SENDER_ID","appId":"APP_ID"};
+export const firebaseConfig = {
+  "projectId": "studio-5662297171-b975b",
+  "appId": "1:958608290949:web:a7c92d6fa1d50d9e6025ec",
+  "apiKey": "AIzaSyBhN1z36b1Oko90LbmqS6NGp8Pby9Mx6ww",
+  "authDomain": "studio-5662297171-b975b.firebaseapp.com",
+  "storageBucket": "studio-5662297171-b975b.appspot.com",
+  "messagingSenderId": "958608290949"
+};
