ccdbe19e617d44295654eaaa6c7ca30b79acfce9
Kunal Jaiswal <kunaljaiswal1nov@gmail.com>
2026-01-27 18:38:42 +0000

    bro i have removed auth then why is even there an auth error

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,10 +3,10 @@
 //
 // These details will be automatically populated with a call to `RequestFirebaseBackendTool`.
 export const firebaseConfig = {
-  "projectId": "studio-5662297171-b975b",
-  "appId": "1:958608290949:web:a7c92d6fa1d50d9e6025ec",
-  "apiKey": "AIzaSyBhN1z36b1Oko90LbmqS6NGp8Pby9Mx6ww",
-  "authDomain": "studio-5662297171-b975b.firebaseapp.com",
-  "storageBucket": "studio-5662297171-b975b.appspot.com",
-  "messagingSenderId": "958608290949"
+  projectId: "studio-5662297171-b975b",
+  appId: "1:958608290949:web:a7c92d6fa1d50d9e6025ec",
+  apiKey: "AIzaSyBhN1z36b1Oko90LbmqS6NGp8Pby9Mx6ww",
+  authDomain: "studio-5662297171-b975b.firebaseapp.com",
+  storageBucket: "studio-5662297171-b975b.appspot.com",
+  messagingSenderId: "958608290949"
 };
