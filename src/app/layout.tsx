29dc386064c844bf5f93e039ffc605e93387686e
Kunal Jaiswal <kunaljaiswal1nov@gmail.com>
2026-01-29 08:12:18 +0000

    In order to pass this test you are advised to use a CDN service. A Conte

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,16 @@
 import type {Metadata} from 'next';
+import { Inter } from 'next/font/google';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import { FirebaseProvider } from '@/firebase/provider';
 import { UserProvider } from '@/contexts/user-context';
 import { Analytics } from "@vercel/analytics/react";
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+});
 
 export const metadata: Metadata = {
   title: 'RandomTalk: Voice Chat with Strangers',
@@ -30,12 +37,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
-      </head>
+    <html lang="en" className={`${inter.variable} dark`}>
+      <head />
       <body className="font-body antialiased">
         <FirebaseProvider>
           <UserProvider>
